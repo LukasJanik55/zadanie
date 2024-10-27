2605b5ad500b329fd7a1677dead9d1904433143a
Lukas Janik <lukas.janik11@gmail.com>
2024-10-27 11:46:53 +0100

    Removed DamagedBook and DamagedSignedBook classes;

--- src/book/DamagedBook.java
+++
@@ -1,9 +0,0 @@
-package book;
-
-public class DamagedBook extends PhysicalBook {
-
-    public DamagedBook(String title, String author, int year, String isbn, int value, int numberOfPages) {
-        super(title, author, year, isbn, value, numberOfPages);
-    }
-
-}
--- src/book/DamagedSignedBook.java
+++
@@ -1,9 +0,0 @@
-package book;
-
-public class DamagedSignedBook extends SignedBook {
-
-    public DamagedSignedBook(String title, String author, int year, String isbn, int value, int numberOfPages) {
-        super(title, author, year, isbn, value, numberOfPages);
-    }
-
-}
